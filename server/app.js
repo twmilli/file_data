842305dc1e4b70cf2e3321e98f580e13039d3644
Taylor Milliman <twm013@bucknell.edu>
2016-11-20 22:03:26 +0000

    a

--- server/app.js
+++ server/app.js
@@ -1,8 +1,9 @@
 import express from 'express';
+import path from 'path';
 import router from './router.js';
 
 export const app = express();
-app.use('/', express.static(__dirname + '/../View'));
+app.use('/', express.static(path.join(__dirname,'../View')));
 
 /**
  * parses text as url encoded data and exposes resulting object)
