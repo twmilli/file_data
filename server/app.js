f345d28652bcbafde33bbe1dc23041700a196d81
Taylor Milliman <twm013@bucknell.edu>
2016-11-20 21:58:41 +0000

    hopefully fixed

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import router from './router.js';
 
 export const app = express();
-app.use('/', express.static('View'));
+app.use('/', express.static(__dirname + '/../View'));
 
 /**
  * parses text as url encoded data and exposes resulting object)
